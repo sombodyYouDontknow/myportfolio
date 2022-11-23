02dc937d538e5d1b6e1626486bca6480df2a1951
sachin <somebodyoudontknow77@gmail.com>
2022-11-23 15:42:19 +0100

    skills completed

--- Myportfolio/src/components/Skill.jsx
+++ Myportfolio/src/components/Skill.jsx
@@ -1,57 +1,78 @@
 import React, { useEffect } from 'react'
-import {FaHtml5 ,FaCss3,FaJava,FaBootstrap,FaReact,FaPhp } from 'react-icons/fa'
+import { FaHtml5, FaCss3, FaJava, FaBootstrap, FaReact, FaPhp,FaPython } from 'react-icons/fa'
+import {SiMysql} from 'react-icons/si'
 import AOS from 'aos';
 import 'aos/dist/aos.css';
 import './styles/skill.css'
 function Skill() {
+  useEffect(() => {
+    AOS.init();
+  }, [])
   return (
-    <section id="skill">
-        <div className='heading'><h2>The skills I have</h2></div>
-      
-    <div className='skill_container'>
-     <div className='skill_frontend'>
-       <h3>Frontend Development</h3>
-       <div className='skill_content'>
-         <article className='skill_details'>
-            <FaHtml5 style={{color:"white"}}/>
-            <p>HTML</p>
-        </article>  
-        <article className='skill_details'>
-            <FaCss3 style={{color:"white"}}/>
-            <p>CSS</p>
-        </article> 
-        <article className='skill_details'>
-            <FaJava style={{color:"white"}}/>
-            <p>JavaScript</p>
-        </article> 
-        <article className='skill_details'>
-            <FaBootstrap style={{color:"white"}}/>
-            <p>Bootsrap</p>
-        </article> 
-        <article className='skill_details'>
-            <FaReact style={{color:"white"}}/>
-            <p>React</p>
-        </article>     
-       </div> 
-     </div>
+    <div id="skill">
+      <div  data-aos="fade-right" data-aos-duration="1000" className='heading'><h2>The skills I have</h2></div>
+      <div className='skill_container'>
+        <div className='skill_frontend'  data-aos="flip-left" data-aos-duration="1000">
+          <h3>Frontend Development</h3>
+          <div className='skill_content'>
+            <article className='skill_details'>
+              <FaHtml5 style={{ color: "white"}} size={20} className='display-icons'/>
+              <div>
+                <p>HTML</p>
+              </div>
+            </article>
+            <article className='skill_details'>
+              <FaCss3 style={{ color: "white" }} size={20} className='display-icons'/>
+              <div>
+                <p>CSS</p>
+              </div>
+            </article>
+            <article className='skill_details'>
+              <FaJava style={{ color: "white" }} size={20} className='display-icons'/>
+              <div>
+                <p>JavaScript</p>
+              </div>
+            </article>
+            <article className='skill_details'>
+              <FaBootstrap style={{ color: "white" }} size={20} className='display-icons'/>
+              <div>
+                <p>Bootsrap</p>
+              </div>
+            </article>
+            <article className='skill_details'>
+              <FaReact style={{ color: "white" }} size={20} className='display-icons'/>
+              <div>
+                <p>React</p>
+              </div>
+            </article>
+          </div>
+        </div>
 
-     <div className='skill_backend'>
-       <h3>Backend Development</h3>
-       <div className='skill_content'>
-         <article className='skill_details'>
-            <FaPhp style={{color:"white"}}/>
-            <p>PHP Framework</p>
-        </article> 
-        <article className='skill_details'>
-            <FaHtml5 style={{color:"white"}}/>
-            <p>MySQL</p>
-        </article>       
-       </div> 
-     </div>
-
-
+        <div className='skill_backend' data-aos="flip-right" data-aos-duration="1000" >
+          <h3>Backend Development</h3>
+          <div className='skill_content'>
+            <article className='skill_details'>
+              <FaPhp style={{ color: "white" }} size={20} className='display-icons'/>
+              <div>
+                <p>PHP Framework</p>
+              </div>
+            </article>
+            <article className='skill_details'>
+              <SiMysql style={{ color: "white" }} size={30} className='display-icons'/>
+              <div>
+                <p>MySQL</p>
+              </div>
+            </article>
+            <article className='skill_details'>
+              <FaPython style={{ color: "white" }} size={20} className='display-icons'/>
+              <div>
+                <p>Python</p>
+              </div>
+            </article>
+          </div>
+        </div>
+      </div>
     </div>
-    </section>
   )
 }
 
